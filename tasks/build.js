ef656eaf7855e50ee034c128cc01fcba6a9a96f4
Kevin Lactio Kemta <kevinlactiokemta@gmail.com>
2024-08-28 17:21:49 +0200

    update build script

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,12 @@
 import esbuild from "esbuild";
-import { run } from "./utils.js";
-
+import {replaceTextInFile, run} from "./utils.js";
+import copy from "esbuild-plugin-copy";
+import writeFilePlugin from "esbuild-plugin-write-file";
+import _package from "../package.json" with { type: "json" }
+import textReplace from "esbuild-plugin-text-replace";
 Promise.all([
-  run("tsc -p tsconfig.json"),
+  run("tsc -p tsconfig.build.json"),
+  replaceTextInFile("./dist/index.d.ts", '"index"', '"co2m.js"'),
   // bundle for esm
   esbuild.build({
     entryPoints: ["src/lib/index.ts"],
@@ -13,7 +17,7 @@ Promise.all([
     bundle: true,
   }),
 
-  // bundle for commonjs
+  // bundle for commonjs & copy necessary files
   esbuild.build({
     entryPoints: ["src/lib/index.ts"],
     format: "cjs",
@@ -21,6 +25,37 @@ Promise.all([
     minify: false,
     platform: "node",
     outfile: "./dist/index.cjs",
+    plugins: [
+        copy({
+          resolveFrom: "cwd",
+          // dryRun: true,
+          assets: [
+            {
+              from: ["./package.json"],
+              to: ["./dist/package.json"]
+            },
+            {
+              from: ["./LICENSE"],
+              to: ["./dist"]
+            },
+            {
+              from: ["./README.md"],
+              to: ["./dist/README.md"]
+            }
+          ]
+        }),
+        writeFilePlugin({
+          after: {
+            "./dist/version.txt" : `${_package.version}`
+          }
+        }),
+        textReplace({
+          include: /dist\/index\.d\.ts$/,
+          pattern: [
+              ['index', 'co2m.js']
+          ]
+        })
+    ]
   }),
 
   // bundle for browser
@@ -32,4 +67,5 @@ Promise.all([
     outfile: "./dist/index.min.js",
     platform: "browser",
   }),
+
 ]).then();
--- tasks/utils.js
+++ tasks/utils.js
@@ -1,8 +1,11 @@
 import { exec } from "child_process";
+import _package from "../package.json" with { type: "json" }
+import {execSync} from "node:child_process";
+import * as fs from "node:fs";
 export function run(script) {
   return new Promise((resolve, reject) => {
     try {
-      exec(script + "", (error, output) => {
+      execSync(script + "", (error, output) => {
         if (error) {
           // throw error
           console.log(error);
@@ -13,4 +16,19 @@ export function run(script) {
       return reject(e.message);
     }
   });
+}
+
+export function replaceTextInFile(file, find, target) {
+  const content = fs.readFileSync(file, "utf-8");
+  const newContent = content.replace(find, target);
+  return Promise.all([
+      run(`echo '${newContent}' > ${file}`)
+  ])
+}
+
+export function prepareDistDir() {
+  return Promise.all([
+    // run("touch dist/package.json && cat package.json >> dist/package.json"),
+    run(`touch dist/version.txt && echo '${_package.version}' > dist/version.txt`)
+  ])
 }
